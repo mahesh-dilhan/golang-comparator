4e27881b782a1b432c018300e221e2019042931c
mahesh <mail.mahesh.lk@gmail.com>
2021-08-11 20:32:39 +0800

    map comparator

--- map/main.go
+++ map/main.go
@@ -6,15 +6,16 @@ import (
 )
 
 func main() {
-	
-m := map[string]int{"Alice": 2, "Cecil": 1, "Bob": 3}
 
-keys := make([]string, 0, len(m))
-for k := range m {
-keys = append(keys, k)
-}
-sort.Strings(keys)
+	m := map[string]int{"Alice": 2, "Cecil": 1, "Bob": 3}
 
-for _, k := range keys {
-fmt.Println(k, m[k])
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		fmt.Println(k, m[k])
+	}
 }
