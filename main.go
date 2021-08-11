617659686464102e96fb060cf56c2cdae45ca4f4
mahesh <mail.mahesh.lk@gmail.com>
2021-08-11 20:02:32 +0800

    comparator

--- main.go
+++ main.go
@@ -14,4 +14,22 @@ func main() {
 	sort.Strings(s)
 	fmt.Println(s)
 
+	countries := []struct {
+		name          string
+		positiveCases int
+	}{
+		{"USA", 100},
+		{"IND", 10},
+		{"SL", 10},
+		{"ROC", 100},
+		{"CAN", 10},
+	}
+
+	sort.SliceStable(countries, func(i, j int) bool {
+		return countries[i].positiveCases < countries[j].positiveCases
+	})
+
+	sort.Slice(countries, func(i, j int) bool {
+		return countries[i].name < countries[j].name
+	})
 }
