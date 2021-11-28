b067ad80f198dcf0c0e8707f9709870b3bcbc5fc
mahesh <mail.mahesh.lk@gmail.com>
2021-11-28 09:40:29 +0800

    array change

--- equalsop/main.go
+++ equalsop/main.go
@@ -6,38 +6,56 @@ type S struct{}
 
 func main() {
 	var p1, p2 *S
+
+	fmt.Printf("%p%f\n", p1, p1) // nil
+	fmt.Printf("%p%f\n", p2, p2) // nil
+
 	s1 := S{}
 	s2 := S{}
+
 	p1 = &s1
 	p2 = &s2
+
+	fmt.Printf("%s\n", s1)
+	fmt.Printf("%s\n", s2)
 	fmt.Printf("%p\n", p1) // 0x1e52bc
 	fmt.Printf("%p\n", p2) // 0x1e52bc
-	fmt.Println(p1)        // &{}
-	fmt.Println(p2)        // &{}
-	fmt.Println(&p1)       // 0x40c138
-	fmt.Println(&p2)       // 0x40c140
-	fmt.Println(*p1)       // {}
-	fmt.Println(*p2)       // {}
-	fmt.Println(p1 == p2)  // true
+
+	fmt.Println(p1)       // &{}
+	fmt.Println(p2)       // &{}
+	fmt.Println(&p1)      // 0x40c138
+	fmt.Println(&p2)      // 0x40c140
+	fmt.Println(*p1)      // {}
+	fmt.Println(*p2)      // {}
+	fmt.Println(p1 == p2) // true
 	fmt.Println(s1 == s2)
 
-	cn := struct {
+	us := struct {
 		name          string
 		positiveCases int
 	}{
 		"USA", 100,
 	}
 
-	us := struct {
+	cn := struct {
 		name          string
 		positiveCases int
 	}{
 		"CNH", 100,
 	}
 
+	countries := []struct {
+		name          string
+		posativecases int
+	}{
+		{"US", 100},
+		{"RUS", 200},
+	}
+
 	fmt.Println(cn == us)
 	fmt.Println(&cn, &us)
 
+	fmt.Println(countries)
 	var m map[int]string = map[int]string{}
 	fmt.Println(m)
 	var n, q map[int]string
